0805401fd0cc2085847c32c497fd1416f29bda2c
Saamin Cheema <sc46@sanger.ac.uk>
2025-06-11 15:15:36 +0000

    Match sample pairs

--- scripts/prep/generate_sample_pairs.R
+++ scripts/prep/generate_sample_pairs.R
@@ -9,7 +9,9 @@ metadata <- metadata |>
 filtered_metadata <- metadata |>
   group_by(patient_id) |>
   filter(all(c("Precursor", "Follow up") %in% precursor_or_follow_up)) |>
-  ungroup()
+  ungroup() |>
+  filter(!sanger_dna_id %in% c("PD62031d", "PD62037a", "PD62038a",
+                               "PD62064c", "PD62065e", "PD62075e"))
 
 write_tsv(filtered_metadata, "metadata/sample_pairs.tsv")
 
