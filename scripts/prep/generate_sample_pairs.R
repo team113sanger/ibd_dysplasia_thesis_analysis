d5f38c466fff9beba8a1a7bdc8f6d0512fa033ec
Saamin Cheema <sc46@sanger.ac.uk>
2025-06-12 09:50:20 +0000

    Correct pairs

--- scripts/prep/generate_sample_pairs.R
+++ scripts/prep/generate_sample_pairs.R
@@ -10,8 +10,15 @@ filtered_metadata <- metadata |>
   group_by(patient_id) |>
   filter(all(c("Precursor", "Follow up") %in% precursor_or_follow_up)) |>
   ungroup() |>
-  filter(!sanger_dna_id %in% c("PD62031d", "PD62037a", "PD62038a",
-                               "PD62064c", "PD62065e", "PD62075e"))
+  filter(!sanger_dna_id %in% c("PD62031d", "PD62037a", "PD62064c", "PD62065e", "PD62075e")) |>
+  mutate(patient_id = case_when(
+    sanger_dna_id %in% c("PD62038e", "PD62038a") ~ paste0(patient_id, "a"),
+    TRUE ~ patient_id
+  )) |>
+  mutate(precursor_or_follow_up = case_when(
+    sanger_dna_id == "PD62064d" ~ "Follow up",
+    TRUE ~ precursor_or_follow_up
+  ))
 
 write_tsv(filtered_metadata, "metadata/sample_pairs.tsv")
 
