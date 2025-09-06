cf9dd46cf0f455627bba793f692128427a97f4bd
Saamin Cheema <sc46@sanger.ac.uk>
2025-09-06 23:10:14 +0000

    Add split mafs

--- scripts/prep/04.split_maf.R
+++ scripts/prep/04.split_maf.R
@@ -1,6 +1,8 @@
 library(readr)
 library(dplyr)
 
+# For running dndscv
+
 maf <- read_tsv("data/variants/7100_3235-filtered_mutations_matched_indepTum_keep.maf")
 
 prog_pre_list <- read_lines("metadata/sample_lists/progressor_precursor_samples_ppat.tsv")
@@ -8,8 +10,8 @@ n_prog_pre_list <- read_lines("metadata/sample_lists/non_progressor_precursor_sa
 
 prog_pre_maf <- maf |>
   filter(Tumor_Sample_Barcode %in% prog_pre_list)
-write_tsv(prog_pre_maf, "data/mafs/progressors_precursors.maf")
+write_tsv(prog_pre_maf, "data//variants/split_mafs/progressors_precursors.maf")
 
 n_prog_pre_maf <- maf |>
   filter(Tumor_Sample_Barcode %in% n_prog_pre_list)
-write_tsv(n_prog_pre_maf, "data/mafs/non_progressors_precursors.maf")
+write_tsv(n_prog_pre_maf, "data/variants/split_mafs/non_progressors_precursors.maf")
