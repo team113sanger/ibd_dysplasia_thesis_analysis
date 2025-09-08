afe89974693db0842b56ba1d3cfc4cc16a67dbbb
Saamin Cheema <sc46@sanger.ac.uk>
2025-09-08 20:32:41 +0000

    Update mafs

--- scripts/01.prep/04.split_maf.R
+++ scripts/01.prep/04.split_maf.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 # For running dndscv
 
-maf <- read_tsv("data/variants/7100_3235-filtered_mutations_matched_indepTum_keep.maf")
+maf <- read_tsv("data/variants/7100_3235-filtered_mutations_matched_allTum_keep.maf")
 
 prog_pre_list <- read_lines("metadata/sample_lists/progressor_precursor_samples_ppat.tsv")
 n_prog_pre_list <- read_lines("metadata/sample_lists/non_progressor_precursor_samples_ppat.tsv")
