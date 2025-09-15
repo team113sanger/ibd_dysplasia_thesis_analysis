e2a42ae8deb9394f527d3a46c1d60e9156a78f54
Saamin Cheema <sc46@sanger.ac.uk>
2025-09-15 22:07:59 +0000

    Dndscv plots

--- scripts/plot/dndscv/plot_dndscv.R
+++ scripts/plot/dndscv/plot_dndscv.R
@@ -2,7 +2,6 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(forcats)
 library(patchwork)
 
 #### Prep ####
@@ -11,18 +10,22 @@ prog <- read_tsv("data/variants/dndscv/dndscv_genes_prog_pre.tsv") |>
   mutate(group = "Progressor")
 non_prog <- read_tsv("data/variants/dndscv/dndscv_genes_non_prog_pre.tsv") |>
   mutate(group = "Non-Progressor")
-prog_ci <- read_tsv("data/variants/dndscv/prog_gene_ci.tsv") |>
+prog_ci <- read_tsv("data/variants/dndscv/prog_pre_gene_ci.tsv") |>
   mutate(group = "Progressor")
-non_prog_ci <- read_tsv("data/variants/dndscv/non_prog_gene_ci.tsv") |>
+non_prog_ci <- read_tsv("data/variants/dndscv/non_prog_pre_gene_ci.tsv") |>
   mutate(group = "Non-Progressor")
 
-combined_df <- bind_rows(prog, non_prog)
+# combine dfs
+combined_muts <- bind_rows(prog, non_prog)
+combine_ci <- bind_rows(prog_ci, non_prog_ci) |>
+  rename(gene_name = gene) 
 
+# n in each group
 n_prog <- 19
 n_nprog <- 21
 
 # Reshape and filter data
-muts_df <- combined_df |>
+muts_df <- combined_muts |>
   select(gene_name, qglobal_cv, group, n_syn, n_mis, n_non, n_spl, n_ind) |>
   pivot_longer(
     cols = starts_with("n_"),
@@ -36,19 +39,36 @@ muts_df <- combined_df |>
     )
   )
 
+ci_df <- combine_ci |>
+  tidyr::pivot_longer(cols = starts_with(c("mis", "tru")),
+                      names_to = c("type", ".value"),
+                      names_pattern = "(mis|tru)_(.*)") |>
+  mutate(type = dplyr::recode(type,
+                         "mis" = "Missense",
+                         "tru" = "Truncating")
+        ) |>
+  rename(dnds = mle)
+
 # Only significant genes
 sig_muts_sums <- muts_df |>
   filter(qglobal_cv < 0.05) |>
   group_by(gene_name, group, mutation_type) |>
   summarise(total = sum(count), .groups = "drop")
 
+# Get genes for plot
+genes_to_plot <- sig_muts_sums |>
+  group_by(gene_name) |>
+  arrange(desc(total)) |>
+  pull(gene_name) |>
+  unique()
+
+# Get percentage mutated
 gene_pct <- sig_muts_sums |>
   group_by(group, gene_name) |>
   summarise(total_mut = sum(total), .groups = "drop") |>
   mutate(percent_samples = round(100 * total_mut / ifelse(group == "Progressor", n_prog, n_nprog), 1))
 
 ##### Plot ##### 
-### Barplot ###
 # Define colours
 mutation_colours <- c(
   "Synonymous" = "#8DA0CB",   # slate blue-grey
@@ -58,7 +78,7 @@ mutation_colours <- c(
   "Indel"      = "#7d6a56ff"    # teal
 )
 
-# Plot
+# Mutation count barplot
 counts_barplot <- ggplot(sig_muts_sums, aes(x = reorder(gene_name, -total), y = total, fill = mutation_type)) +
   geom_col() +
   geom_text(
@@ -67,7 +87,7 @@ counts_barplot <- ggplot(sig_muts_sums, aes(x = reorder(gene_name, -total), y = 
     inherit.aes = FALSE, 
     vjust = 1, size = 3, angle = 45
   ) +
-  facet_wrap(~group, scales = "free_x", strip.position = "top") +
+  facet_wrap(~group, scales = "free_x", ncol =1, strip.position = "top") +
   scale_fill_manual(values = mutation_colours) +
   labs(
     x = NULL,
@@ -90,77 +110,33 @@ counts_barplot <- ggplot(sig_muts_sums, aes(x = reorder(gene_name, -total), y = 
   ) +
   scale_y_continuous(limits = c(0,20))
 
-ggsave("plots/dndscv/precursors/dndscv_barplot.png",
-      plot = counts_barplot, height = 3, width = 4.7, dpi = 300)
+ggsave(
+  "plots/dndscv/precursors/dndscv_counts_barplot.png",
+  plot = counts_barplot, height = 5.5, width = 3.5, dpi = 300
+)
 
-### TP53 Plot ###
-tp53_df <- muts_df |>
-  filter(gene_name == "TP53") |>
-  group_by(gene_name, group, mutation_type) |>
-  summarise(total = sum(count), .groups = "drop")
+# dnds ratios barplot
+# dnds_plot <- ggplot(ci_df, aes(x = gene_name, y = dnds, fill = type)) +
+#   geom_col(position = position_dodge(width = 0.9)) +
+#   geom_errorbar(
+#     aes(ymin = low, ymax = high),
+#     width = 0.3,
+#     position = position_dodge(width = 0.9)
+#   ) +
+#   facet_wrap(~group, ncol = 1) +
+#   scale_y_log10() +
+#   scale_fill_brewer(palette = "Set2") +
+#   labs(
+#     x = "Gene",
+#     y = "dN/dS (log scale)",
+#     fill = NULL
+#   ) +
+#   theme_classic(base_size = 12) +
+#   theme(
+#     axis.text.x = element_text(angle = 45, hjust = 1, face = "italic"),
+#     strip.text = element_text(face = "bold"),
+#     legend.position = "right"
+#   )
 
-barplot <- ggplot(tp53_df, aes(x = gene_name, y = total, fill = mutation_type)) +
-              geom_col(position = "stack", width = 0.7) +
-              facet_wrap(~group, scales = "free_x") +
-              scale_fill_manual(values = mutation_colours) +
-              labs(
-                x = "TP53",
-                y = "Mutation Count",
-                fill = "Mutation Type"
-              ) +
-              theme_classic(base_size = 13) +
-              theme(
-                panel.grid = element_blank(),
-                axis.line.y = element_line(colour = "black"),
-                axis.line.x = element_blank(),
-                axis.title.x = element_text(face = "italic"),
-                axis.ticks.x = element_blank(),
-                axis.text.x = element_blank(),
-                strip.background = element_blank(),
-                strip.text = element_blank(),
-                legend.position = "right",
-                legend.key.size = unit(0.4, "cm"),
-                legend.text = element_text(size = 9),
-                legend.title = element_text(size = 9)
-              )
-
-# Prep
-ci_df <- bind_rows(prog_ci, non_prog_ci) |>
-  rename(gene_name = gene) |>
-  tidyr::pivot_longer(cols = starts_with(c("mis", "tru")),
-                      names_to = c("type", ".value"),
-                      names_pattern = "(mis|tru)_(.*)") |>
-  mutate(type = dplyr::recode(type,
-                         "mis" = "Missense",
-                         "tru" = "Truncating")
-        )
-
-# Dot plot of truncating dN/dS with error bars
-dotplot <- ggplot(ci_df, aes(x = gene_name, y = mle, colour = type)) +
-  geom_point(size = 2, position = position_dodge(width = 0.7)) +
-  geom_errorbar(aes(ymin = low, ymax = high),
-    width = 0.3,
-    position = position_dodge(width = 0.7)
-  ) +
-  geom_hline(yintercept = 1, linetype = "dashed", colour = "grey30") +
-  scale_color_manual(values = c("#68a334ff", "#9f2727ff")) +
-  scale_y_log10(trans = "pseudo_log", breaks = c(1, 10, 100, 1000, 10000)) +
-  facet_wrap(~group, scales = "free_x") +
-  theme_classic(base_size = 13) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.line.x = element_blank(),
-    strip.background = element_blank(),
-    strip.text = element_text(face = "bold"),
-    legend.text = element_text(size = 9),
-    legend.title = element_text(size = 9),
-    legend.position = "right"
-  ) +
-  labs(y = "dN/dS", colour = "Mutation Type")
-
-# Combine
-combined_plot <- dotplot / barplot + plot_layout(heights = c(1, 1.5))
-
-ggsave("plots/dndscv/precursors/dndscv_tp53_plot.png", plot = combined_plot, height = 5, width = 5.2, dpi = 300)
+# ggsave("plots/dndscv/precursors/dnds_ratios_barplot.png",
+#        plot = dnds_plot, height = 5, width = 6, dpi = 300)
---
+++ scripts/plot/dndscv/plot_drivers_nprog.R
@@ -0,0 +1,112 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(patchwork)
+
+#### Prep ####
+# Read in data
+nprog <- read_tsv("data/variants/dndscv/dndscv_genes_non_prog_pre.tsv")
+nprog_ci <- read_tsv("data/variants/dndscv/non_prog_pre_gene_ci.tsv") |>
+    filter(!gene == "TP53")
+
+# Filter and reshape data
+muts_df <- nprog |>
+  select(gene_name, n_syn, n_mis, n_non, n_spl, n_ind, qglobal_cv) |>
+  filter(qglobal_cv < 0.05) |>
+  pivot_longer(
+    cols = starts_with("n_"),
+    names_to = "mutation_type",
+    values_to = "count"
+  ) |>
+  mutate(
+    mutation_type = recode(mutation_type,
+      n_syn = "Synonymous", n_mis = "Missense", n_non = "Nonsense",
+      n_spl = "Splice", n_ind = "Indel"
+    )
+  )
+
+ci_df <- nprog_ci |>
+  rename(gene_name = gene) |>
+  tidyr::pivot_longer(cols = starts_with(c("mis", "tru")),
+                      names_to = c("type", ".value"),
+                      names_pattern = "(mis|tru)_(.*)") |>
+  mutate(type = dplyr::recode(type,
+                         "mis" = "Missense",
+                         "tru" = "Truncating")
+        )
+
+# Set order of genes
+genes_order <- muts_df |>
+    arrange(qglobal_cv) |>
+    pull(gene_name) |>
+    unique()
+
+muts_df <- muts_df |>
+    mutate(gene_name = factor(gene_name, labels = genes_order))
+
+ci_df <- ci_df |>
+    mutate(gene_name = factor(gene_name, labels = genes_order))
+
+
+# Define colour palette
+mutation_colours <- c(
+  "Missense"   = "#8cb87aff",  
+  "Truncating" = "#7c679eff",
+  "Indel" = "#77a9baff",
+  "Nonsense" = "#7c679eff",
+  "Splice" = "#d5ad5eff",
+  "Synonymous" ="#696a6bff"
+)
+
+# Counts barplot
+counts_barplot <- ggplot(muts_df, aes(x = gene_name, y = count, fill = mutation_type)) +
+  geom_col(width = 0.8) +
+  scale_fill_manual(values = mutation_colours) +
+  labs(
+    x = NULL,
+    y = "Total Mutations",
+    fill = NULL
+  ) +
+  theme_classic(base_size = 8) +
+  theme(
+    axis.text.x = element_blank(),
+    axis.line.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = "right",
+    legend.text  = element_text(size = 6),
+    legend.key.size = unit(0.3, "cm")  
+  )
+
+# dnds ratios barplot
+# Plot
+ci_barplot <- ggplot(ci_df, aes(x = gene_name, y = mle, fill = type)) +
+  geom_col(position = position_dodge(width = 0.8), width = 0.7) +
+  geom_errorbar(
+    aes(ymin = low, ymax = high),
+    position = position_dodge(width = 0.8),
+    width = 0.3
+  ) +
+    geom_hline(yintercept = 1, linetype = "dashed", colour = "grey30") +
+  scale_fill_manual(values = mutation_colours) +
+  scale_y_log10(trans = "pseudo_log", breaks = c(1, 10, 100, 1000, 10000)) +
+  labs(
+    x = NULL,
+    y = "dN/dS ratio",
+    fill = NULL
+  ) +
+  theme_classic(base_size = 8) +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, face = "italic"),
+    axis.line.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = "right",
+    legend.text  = element_text(size = 6),
+    legend.key.size = unit(0.3, "cm")
+  )
+
+# Combine plots
+combined_plot <- counts_barplot / ci_barplot + plot_layout(heights = c(1, 1))
+
+ggsave("plots/dndscv/precursors/dndscv_n_progressors_plot.png",
+        plot = combined_plot, height = 3.5, width = 3.2, dpi = 300)
