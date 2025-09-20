d8366bcde7b292185025fd4880d816303aa51531
Saamin Cheema <sc46@sanger.ac.uk>
2025-09-20 16:01:04 +0000

    Rearrange script so prints tmb values in order

--- scripts/extract/get_median_tmb.R
+++ scripts/extract/get_median_tmb.R
@@ -5,15 +5,15 @@ tmb <- read_tsv("data/variants/mutations_per_Mb.tsv", col_names = c("sample", "t
   left_join(read_tsv("metadata/final_metadata_qc_pass.tsv"), by = c("sample" = "sanger_dna_id")) |>
   filter(sample %in% read_lines("metadata/sample_lists/all_one_ppat.list"))
 
-# Median TMB for LGD Progressors
-median_LGD_prog <- tmb |>
-  filter(grade_of_dysplasia == "Low grade", group == "Progressor") |>
-  summarise(median_tmb = median(tmb)) |>
-  pull(median_tmb)
-
 # Median TMB for LGD Non-Progressors
 median_LGD_nonprog <- tmb |>
   filter(grade_of_dysplasia == "Low grade", group == "Non-progressor") |>
+  summarise(median_tmb = median(tmb)) |>
+  pull(median_tmb)
+
+# Median TMB for LGD Progressors
+median_LGD_prog <- tmb |>
+  filter(grade_of_dysplasia == "Low grade", group == "Progressor") |>
   summarise(median_tmb = median(tmb)) |>
   pull(median_tmb)
 
@@ -30,7 +30,7 @@ median_AC_prog <- tmb |>
   pull(median_tmb)
 
 # Print results
-median_LGD_prog
 median_LGD_nonprog
+median_LGD_prog
 median_HGD_prog
 median_AC_prog
