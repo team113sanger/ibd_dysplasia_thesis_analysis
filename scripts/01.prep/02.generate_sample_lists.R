23fd05f35fb1288fb345b81329bc57bd3bb4e33a
Saamin Cheema <sc46@sanger.ac.uk>
2026-03-25 15:36:21 +0000

    Add study_id to sample lists

--- scripts/01.prep/02.generate_sample_lists.R
+++ scripts/01.prep/02.generate_sample_lists.R
@@ -15,7 +15,7 @@ get_samples <- function(metadata_df, filter_variable, filter_value,
   }
 
   sample_list <- filtered_df |>
-    pull(sanger_dna_id)
+    select(sanger_dna_id, study_id)
 
   return(sample_list)
 }
@@ -30,86 +30,86 @@ extra_samples <- c("PD62065a", "PD62065e", "PD62068d", "PD62068e", "PD62075c", "
 ## Progressors ##
 # All
 progressors <- get_samples(meta, group, "Progressor")
-write_lines(progressors, file.path(outdir, "progressor_samples.tsv"))
+write_tsv(progressors, file.path(outdir, "progressor_samples.tsv"))
 
 progressors_ppat <- get_samples(meta, group, "Progressor") |>
-  setdiff(extra_samples)
-write_lines(progressors_ppat, file.path(outdir, "progressor_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(progressors_ppat, file.path(outdir, "progressor_samples_ppat.tsv"))
 
 # Precursors
 progressor_precursors <- get_samples(
   meta, group, "Progressor",
   precursor_or_follow_up, "Precursor"
 )
-write_lines(progressor_precursors, file.path(outdir, "progressor_precursor_samples.tsv"))
+write_tsv(progressor_precursors, file.path(outdir, "progressor_precursor_samples.tsv"))
 
 progressor_precursors_ppat <- progressor_precursors |> 
-  setdiff(extra_samples)
-write_lines(progressor_precursors_ppat, file.path(outdir, "progressor_precursor_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(progressor_precursors_ppat, file.path(outdir, "progressor_precursor_samples_ppat.tsv"))
 
 # Follow Ups
 progressor_follow_ups <- get_samples(
   meta, group, "Progressor",
   precursor_or_follow_up, "Follow up"
 )
-write_lines(progressor_follow_ups, file.path(outdir, "progressor_follow_up_samples.tsv"))
+write_tsv(progressor_follow_ups, file.path(outdir, "progressor_follow_up_samples.tsv"))
 
 progressor_follow_ups_ppat <- progressor_follow_ups |> 
-  setdiff(extra_samples)
-write_lines(progressor_follow_ups_ppat, file.path(outdir, "progressor_follow_up_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(progressor_follow_ups_ppat, file.path(outdir, "progressor_follow_up_samples_ppat.tsv"))
 
 ## Non-progressors ##
 # All
 non_progressors <- get_samples(meta, group, "Non-progressor")
-write_lines(non_progressors, file.path(outdir, "non_progressor_samples.tsv"))
+write_tsv(non_progressors, file.path(outdir, "non_progressor_samples.tsv"))
 
 non_progressors_ppat <- non_progressors |> 
-  setdiff(extra_samples)
-write_lines(non_progressors_ppat, file.path(outdir, "non_progressor_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(non_progressors_ppat, file.path(outdir, "non_progressor_samples_ppat.tsv"))
 
 # Precursors
 non_progressor_precursors <- get_samples(
   meta, group, "Non-progressor",
   precursor_or_follow_up, "Precursor"
 )
-write_lines(non_progressor_precursors, file.path(outdir, "non_progressor_precursor_samples.tsv"))
+write_tsv(non_progressor_precursors, file.path(outdir, "non_progressor_precursor_samples.tsv"))
 
 non_progressor_precursors_ppat <- non_progressor_precursors |> 
-  setdiff(extra_samples)
-write_lines(non_progressor_precursors_ppat, file.path(outdir, "non_progressor_precursor_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(non_progressor_precursors_ppat, file.path(outdir, "non_progressor_precursor_samples_ppat.tsv"))
 
 # Follow Ups
 non_progressor_follow_ups <- get_samples(
   meta, group, "Non-progressor",
   precursor_or_follow_up, "Follow up"
 )
-write_lines(non_progressor_follow_ups, file.path(outdir, "non_progressor_follow_up_samples.tsv"))
+write_tsv(non_progressor_follow_ups, file.path(outdir, "non_progressor_follow_up_samples.tsv"))
 
 non_progressor_follow_ups_ppat <- non_progressor_follow_ups |> 
-  setdiff(extra_samples)
-write_lines(non_progressor_follow_ups_ppat, file.path(outdir, "non_progressor_follow_up_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(non_progressor_follow_ups_ppat, file.path(outdir, "non_progressor_follow_up_samples_ppat.tsv"))
 
 #### Sample lists by grade ####
 low_grade <- get_samples(meta, grade_of_dysplasia, "Low grade")
-write_lines(low_grade, file.path(outdir, "low_grade_samples.tsv"))
+write_tsv(low_grade, file.path(outdir, "low_grade_samples.tsv"))
 
 low_grade_ppat <- low_grade |> 
-  setdiff(extra_samples)
-write_lines(low_grade_ppat, file.path(outdir, "low_grade_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(low_grade_ppat, file.path(outdir, "low_grade_samples_ppat.tsv"))
 
 high_grade <- get_samples(meta, grade_of_dysplasia, "High grade")
-write_lines(high_grade, file.path(outdir, "high_grade_samples.tsv"))
+write_tsv(high_grade, file.path(outdir, "high_grade_samples.tsv"))
 
 high_grade_ppat <- high_grade |> 
-  setdiff(extra_samples)
-write_lines(high_grade_ppat, file.path(outdir, "high_grade_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(high_grade_ppat, file.path(outdir, "high_grade_samples_ppat.tsv"))
 
 carinoma <- get_samples(meta, grade_of_dysplasia, "Adenocarcinoma")
-write_lines(carinoma, file.path(outdir, "adenocarcinoma_samples.tsv"))
+write_tsv(carinoma, file.path(outdir, "adenocarcinoma_samples.tsv"))
 
 carinoma_ppat <- carinoma |> 
-  setdiff(extra_samples)
-write_lines(carinoma_ppat, file.path(outdir, "adenocarcinoma_samples_ppat.tsv"))
+  filter(!sanger_dna_id %in% extra_samples)
+write_tsv(carinoma_ppat, file.path(outdir, "adenocarcinoma_samples_ppat.tsv"))
 
-all_ppat <- c(non_progressors_ppat, progressors_ppat) |>
-  write_lines(file.path(outdir, "all_one_ppat.list"))
+all_ppat <- rbind(non_progressors_ppat, progressors_ppat) |>
+  write_tsv(file.path(outdir, "all_one_ppat.list"))
